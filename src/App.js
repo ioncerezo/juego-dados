27b43ff31acbb1935c36256c162397ae75bd6c69
Ion Cerezo <ioncerezovalero@gmail.com>
2023-02-03 15:44:20 +0000

    Create rules button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [showRules, setShowRules] = useState(false);
   return (
     <div>
-      <h1>Hola</h1>
-      <p>Esta es el app del dado</p>
+      <h1>Holad</h1>
+      <button onClick={()=>setShowRules(!showRules)}>Mostrar reglas</button>
+      <button>New Dice</button>
+      {showRules && (
+        <div>
+          <h3>Reglas</h3>
+          <p>reglas reglas</p>
+        </div>
+      )}
+      
     </div>
   );
 }
